2d5e756bb636e2f89d785e3aa5348a9dd1452a4a
Gaurav Kumar <indiagauravkumar@gmail.com>
2023-08-18 08:54:05 +0530

    rebrand

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
           height={30}
           className='object-contain'
         />
-        <p className='logo_text'>Promptopia</p>
+        <p className='logo_text'>Post<span className='orange_gradient text-center'>!T</span></p>
       </Link>
 
       {/* Desktop Navigation */}
