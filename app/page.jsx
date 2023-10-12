7fa81d1e631c6630c9297f149d7ac1eecfc61eb7
Gaurav kumar <68615047+i-m-Gaurav@users.noreply.github.com>
2023-10-12 22:43:16 +0000

    front page updated

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,12 @@ import Feed from "@components/Feed";
 const Home = () => (
   <section className='w-full flex-center flex-col'>
     <h1 className='head_text text-center'>
-      Write & Share
+      Discover & Share
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> Fun posts</span>
+      <span className='orange_gradient text-center'> Creative prompts</span>
     </h1>
     <p className='desc text-center'>
-      Post<span className='orange_gradient text-center'>!T</span> is an open-source place for sharing and discovering fun posts. </p>
+      Prompt<span className='orange_gradient text-center'>Store</span> is an open-source prompting tool to discover, create and share creative prompts for modern world.</p>
 
     <Feed />
   </section>
